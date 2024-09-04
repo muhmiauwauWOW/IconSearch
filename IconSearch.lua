2074bc34bc5d511b0fe9c18b91ee9257dafd9b61
muhmiauwau <fabio@madless.de>
2024-09-04 12:05:37 +0200

    section search

--- IconSearch.lua
+++ IconSearch.lua
@@ -1,3 +1,5 @@
+
+IconSearchAddon = LibStub("AceAddon-3.0"):NewAddon("IconSearch", "AceEvent-3.0")
 local _ = LibStub("LibLodash-1"):Get()
 
 
@@ -106,7 +108,26 @@ end
 
 
 
+IconSearchButtonMixin = {}
 
+
+function IconSearchButtonMixin:OnClick()
+
+	
+end
+
+function IconSearchButtonMixin:OnEnter()
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	GameTooltip:AddLine(self.data.name)
+	GameTooltip:AddDoubleLine("Type:",self.data.type)
+	GameTooltip:AddDoubleLine("texture:",self.data.texture)
+	GameTooltip:AddLine(self.data.search)
+	GameTooltip:Show()
+end
+
+function IconSearchButtonMixin:OnLeave()
+	GameTooltip:Hide();
+end
 
 
 
@@ -122,33 +143,86 @@ function IconSectionSelectorMixin:OnLoad()
 	self.Data = ICONSEARCH_DATA
 
 
-
-	self.Contents:SetHeight(1111)
-
-
-	self:Show()
-
 	self.pool = CreateFramePool("Frame", self.Contents, "IconSelectorSectionTemplate")
 
 
 
+	IconSearchAddon:RegisterMessage("search", function(self, search, searchString)
+		for widget in self.pool:EnumerateActive() do
+			
+			local data = _.filter(widget.IconSelector.data, function(icon) 
+				return string.find(string.lower(icon.search), searchString)
+			end)
+
+			widget:SetShown(#data > 0)
+			widget.IconSelector:renderIcons(data)
+		end
+
+		self:calcHeight()
+	end, self)
+
+	IconSearchAddon:RegisterMessage("reset", function(self, reset, searchString)
+		for widget in self.pool:EnumerateActive() do
+			widget.IconSelector:renderIcons(widget.IconSelector.data)
+			widget:Show()
+		end
+
+		self:calcHeight()
+	end, self)
+
+end
+
+
+
+function IconSectionSelectorMixin:buildSections()
+	self.pool:ReleaseAll()
 	_.forEach(self.Data.sections, function(section,idx)
-		print(section.name,idx)
-		local pos = ((idx - 1) * (36 + 360)) *-1
+		local pos = ((idx - 1) * (136)) *-1
 		local frame = self.pool:Acquire();
-		frame:SetHeight(100)
+		frame.name = section.name
+		frame.height = 36
+		frame.IconSelector.initialized = false
+		print(section.name, #section.obj)
 		frame.IconSelector.data = section.obj
 		frame.Title:SetText(section.name);
-		frame:SetPoint("TOPLEFT", self.Contents, "TOPLEFT", 0, pos)
-		frame:Show()
+		frame:SetShown(#section.obj > 0)
+
+		-- frame.Texture = frame:CreateTexture()
+		-- frame.Texture:SetAllPoints()
+		-- frame.Texture:SetColorTexture(0,0, 0, 0.7)
+	end)
+end
+
+
+
+function IconSectionSelectorMixin:OnShow()
+	DevTool:AddData(ICONSEARCH_DATA, "ICONSEARCH_DATA")
+	self.Data = ICONSEARCH_DATA
+	self:buildSections()
+end
+
+function IconSectionSelectorMixin:calcHeight()
+	local height = 0
+	local children = {self.Contents:GetChildren()}
+-- print("ddd calcHeight")
+-- 	for widget in self.pool:EnumerateActive() do
+-- 		print(widget.name)
+-- 		widget:SetPoint("TOPLEFT", 0, height * -1)
+-- 		height = height + widget.height
+-- 	end
+
+
+	table.foreach(children, function(idx, child)
+		if not child:IsShown() then return end
+		child:SetPoint("TOPLEFT", 0, height * -1)
+		height = height + child.height
 	end)
 
-	
 
-
-
-	print("vvvv")
+	self.Contents:SetHeight(height)
 end
+
+
 
 
 
@@ -157,38 +231,77 @@ IconSelectorSectionMixin = {}
 
 
 function IconSelectorSectionMixin:OnLoad()
-	print("IconSelectorSectionMixin")
 end
 
+
+
+function IconSelectorSectionMixin:setHeight(height)
+	height = height + 40
+	self.height = height
+	self:SetHeight(height)
+	
+end
 
 
 
 IconSelectorMixin = {}
-function IconSelectorMixin:OnShow()
-	if not self.initialized then
-		self:Init();
-	end
+function IconSelectorMixin:OnLoad()
+	self.data = {}
+	self.initialized = false;
+	self.pool = CreateFramePool("FRAME", self, "IconSearchButtonTemplate")
 
-	print("IconSelectorMixin")
 end
 
 
-
+function IconSelectorMixin:OnShow()
+	if not self.initialized then
+		print("init")
+		self:Init();
+	end
+end
 
 function IconSelectorMixin:Init()
 
-	local view = CreateScrollBoxListGridView(10);
+	self.cols = 10
+	self.padding = 45 + 1
 
-	local function InitializeGridSelectorScrollButton(button, selectionIndex)
-		self:RunSetup(button, selectionIndex);
+	self.getPos = function(type, index)
+		index = index -1
+		local row = math.floor(index / self.cols) 
+		if type == "y" then return row * self.padding end
+		return (index - (row * self.cols)) *  self.padding
 	end
+	print(#self.data)
 
-	local templateType, buttonTemplate = self:GetButtonTemplate();
-	view:SetElementInitializer(buttonTemplate, InitializeGridSelectorScrollButton);
-
+	self:renderIcons()
+	self:GetParent():GetParent():GetParent():calcHeight()
+	self.initialized = true;
 end
 
 
+
+function IconSelectorMixin:renderIcons(data)
+	self.pool:ReleaseAll()
+	data = data or self.data
+	_.forEach(data, function(entry, idx)
+		local x = 0
+		local y = 0
+		local frame = self.pool:Acquire();
+		
+		
+		frame:SetPoint("TOPLEFT", self, "TOPLEFT", self.getPos("x", idx), 0 - self.getPos("y", idx))
+		frame:SetHeight(45)
+		frame:SetWidth(45)
+		frame.data = entry
+		frame.button.data = entry
+		frame.button.Icon:SetTexture(entry.texture);
+		frame:Show()
+	end)
+
+	local height = #data == 0 and 45 or ( math.ceil(#data / self.cols) * self.padding )
+	self.NoSearchResultsText:SetShown(#data == 0)
+	self:GetParent():setHeight(height)
+end
 
 
 
@@ -211,21 +324,23 @@ IconSearchTitleTemplateTitleMixin = {}
 
 
 
-
 IconSearchSearchBarMixin = {}
 
 function IconSearchSearchBarMixin:OnLoad()
 	SearchBoxTemplate_OnLoad(self);
 	self.clearButton:HookScript("OnClick", function(btn)
-		self:GetParent():reset()
+	--	self:GetParent():reset()
+		IconSearchAddon:SendMessage("reset", true)
 		SearchBoxTemplateClearButton_OnClick(btn);
 	end)
 end
 function IconSearchSearchBarMixin:search(text)
     if string.len(text) == 0 then
-		self:GetParent():reset()
+		--self:GetParent():reset()
+		IconSearchAddon:SendMessage("reset", true)
 	else
-		self:GetParent():search(text)
+	--	self:GetParent():search(text)
+		IconSearchAddon:SendMessage("search", text)
 	end
 end
 
@@ -243,20 +358,3 @@ function IconSearchSearchBarMixin:Reset()
 end
 
 
-IconSearchButtonMixin = {}
-
-
-
-
-function IconSearchButtonMixin:OnEnter()
-	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-	GameTooltip:AddLine(self.data.name)
-	GameTooltip:AddDoubleLine("Type:",self.data.type)
-	GameTooltip:AddDoubleLine("texture:",self.data.texture)
-	GameTooltip:AddLine(self.data.search)
-	GameTooltip:Show()
-end
-
-function IconSearchButtonMixin:OnLeave()
-	GameTooltip:Hide();
-end
